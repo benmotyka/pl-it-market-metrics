3631f3d1f222b34b04210f29c52c6f4debff9cc3
benmotyka <benmotykax@gmail.com>
2023-03-31 23:38:20 +0200

    add basic scrappers

--- scrapper/main.py
+++ scrapper/main.py
@@ -2,12 +2,38 @@
 This is scrapper for justjoinit.
 """
 
+from collections import Counter
 import requests
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
 
-response = requests.get("https://news.ycombinator.com/", timeout=5)
+response = requests.get("https://justjoin.it/api/offers/", timeout=10)
 response.raise_for_status()
 
-document = BeautifulSoup(response.content, 'html.parser')
+data = response.json()
 
-print(document)
+overall_positions = len(data)
+
+city_counts = Counter()
+seniority_counts = Counter()
+kind_counts = Counter()
+
+for record in data:
+    city_counts[record['city']] += 1
+    seniority_counts[record['experience_level']] += 1
+    kind_counts[record['marker_icon']] += 1
+
+remote_counts = sum(1 for record in data if record['remote'])
+
+for city, count in city_counts.items():
+    print(f"Localization: there are {count} records for {city}.")
+
+for level, count in seniority_counts.items():
+    print(f"Seniority: there are {count} records for {level}.")
+
+for kind, count in kind_counts.items():
+    print(f"Kind: there are {count} records for {kind}.")
+
+print(f"There are {remote_counts} remote records.")
+
+print(city_counts)
+print(dict(city_counts))
