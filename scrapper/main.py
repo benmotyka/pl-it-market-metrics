cd968ec49e5629e067b82d004ddaf2fd951723bf
benmotyka <benmotykax@gmail.com>
2023-03-31 23:45:11 +0200

    draw basic plot of results

--- scrapper/main.py
+++ scrapper/main.py
@@ -5,6 +5,7 @@ This is scrapper for justjoinit.
 from collections import Counter
 import requests
 # from bs4 import BeautifulSoup
+import matplotlib.pyplot as plt
 
 response = requests.get("https://justjoin.it/api/offers/", timeout=10)
 response.raise_for_status()
@@ -26,6 +27,25 @@ remote_counts = sum(1 for record in data if record['remote'])
 
 for city, count in city_counts.items():
     print(f"Localization: there are {count} records for {city}.")
+
+# Filter the Counter object to only include values greater than 10
+filtered_counter = {key: value for key, value in city_counts.items() if value > 10}
+
+# Sort the dictionary by values in descending order
+sorted_data = sorted(filtered_counter.items(), key=lambda x: x[1], reverse=True)
+
+# Extract the keys and values into separate lists
+cities = [city for city, value in sorted_data]
+counts = [value for city, value in sorted_data]
+
+# Plot the values
+fig, ax = plt.subplots()
+ax.bar([x[0] for x in sorted_data], [x[1] for x in sorted_data])
+ax.set_xlabel('City')
+ax.set_ylabel('Number of job offers')
+ax.set_title('Job offers by city')
+plt.xticks(rotation=90)
+plt.show()
 
 for level, count in seniority_counts.items():
     print(f"Seniority: there are {count} records for {level}.")
