4b04f7ea059d0364428f8e564ae69b47cb9afdc8
benmotyka <benmotykax@gmail.com>
2023-04-19 23:31:37 +0200

    add activity table, change envs in alembic

--- alembic/env.py
+++ alembic/env.py
@@ -1,3 +1,4 @@
+from os import environ
 from logging.config import fileConfig
 from dotenv import load_dotenv
 
@@ -12,6 +13,8 @@ load_dotenv()
 # access to the values within the .ini file in use.
 config = context.config
 
+config.set_main_option('sqlalchemy.url', environ.get("DATABASE_URL"))
+
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
@@ -21,7 +24,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata=None
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -41,7 +44,7 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url=config.get_main_option("sqlalchemy.url")
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -60,7 +63,7 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
+    connectable=engine_from_config(
         config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
