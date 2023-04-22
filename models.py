1d7d638a0c97d87e392b1ba137d495644784203a
benmotyka <benmotykax@gmail.com>
2023-04-22 23:27:00 +0200

    add stats models

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from os import environ
 from datetime import datetime
 from uuid import uuid4
 from dotenv import load_dotenv
-from sqlalchemy import create_engine, Column, String, DateTime
+from sqlalchemy import create_engine, Column, String, DateTime, Integer
 from sqlalchemy.orm import declarative_base, sessionmaker
 from sqlalchemy.dialects.postgresql import UUID
 
@@ -18,6 +18,37 @@ class ActivityModel(Base):
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
     created_at = Column(DateTime, default=datetime.utcnow)
     domain = Column(String(30), nullable=False)
+
+
+class PositionsModel(Base):
+    __tablename__ = 'positions'
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
+    domain = Column(String(30), nullable=False)
+    overall = Column(Integer, nullable=False)
+    remote_count = Column(Integer, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+
+class SeniorityModel(Base):
+    __tablename__ = 'seniority'
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
+    domain = Column(String(30), nullable=False)
+    seniority = Column(String(20), nullable=False)
+    count = Column(Integer, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+
+class LocalizationModel(Base):
+    __tablename__ = 'localization'
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
+    domain = Column(String(30), nullable=False)
+    city = Column(String(50), nullable=False)
+    count = Column(Integer, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
 
 Session = sessionmaker(bind=engine)
 
